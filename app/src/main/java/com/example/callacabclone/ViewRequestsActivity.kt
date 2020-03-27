d9ae164ca119f0bd13e310e8eb56afb01712cecb
vraman3 <vishwanath.raman3@gmail.com>
2020-03-27 17:38:18 +0530

    The tutorial has too many dependencies. It doesn't mention it but it is based on previous source files written. Reverting to an earlier commit.

--- app/src/main/java/com/example/callacabclone/ViewRequestsActivity.kt
+++ app/src/main/java/com/example/callacabclone/ViewRequestsActivity.kt
@@ -8,11 +8,13 @@ import kotlinx.android.synthetic.main.activity_view_requests.*
 class ViewRequestsActivity : AppCompatActivity() {
 
     private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var adapter: RequestRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_requests)
         linearLayoutManager = LinearLayoutManager(this)
         requestsRecyclerView.layoutManager = linearLayoutManager
+//        adapter = RequestRecyclerAdapter(photoList)
     }
 }
